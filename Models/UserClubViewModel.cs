cf023c3fd870d57e983ae75dae395a7ba7645271
MichalisAthanasi <77976079+MichalisAthanasi@users.noreply.github.com>
2021-05-10 23:11:29 +0100

    Add files via upload

---
+++ Models/UserClubViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+namespace Football.Models
+{
+    public class UserClubViewModel
+    {
+        public Club Club { get; set; }
+        public User User { get; set; }
+    }
+
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Football.Startup))]
+namespace Football
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
